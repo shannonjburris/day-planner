19d61ea18d8756643834e06b7d987cdd71aecd04
Shannon Burris <shannonjburris@gmail.com>
2021-07-02 09:50:13 +1000

    fix error for present day

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-
+//Getting current today
     var currentTimeEL = $("#currentDay");
     var timeNow = moment();
     timeNow = moment().format("dddd, MMM DD, YYYY");
     currentTimeEL.text(timeNow);
 
     var saveBtn = $('.saveBtn');
-
+//Empty array to store notes
     var fieldArray = [];
 
     saveBtn.on("click", function () {
@@ -16,17 +16,16 @@ $(document).ready(function () {
 
 
     });
+    //get current hour
     var hour = moment().format("k");
-    console.log("hour " + hour);
     for (let index = 9; index < 18; index++) {
         let savedItem = localStorage.getItem("hour-" + index);
         
         if (savedItem) {
             $(".hour-" + index).val(savedItem);
         }
-        console.log(index);
-
-        if (hour === index) {
+//setting color for hour
+        if (hour == index) {
             $(".hour-" + index).addClass("present"); 
         }
         if (hour < index) {
